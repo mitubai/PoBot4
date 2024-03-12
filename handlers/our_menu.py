05163a867139af128eadaa0acc50af4bb4ac5f14
Igor <igglike@gmail.com>
2024-03-12 19:00:01 +0600

    Добавил текстовые кнопки, и опросник при помощи FSM

---
+++ handlers/our_menu.py
@@ -0,0 +1,38 @@
+from aiogram import Router, F, types
+
+
+menu_router = Router()
+
+@menu_router.callback_query(F.data == "our_menu")
+async def make_order(callback: types.CallbackQuery):
+    kb = types.ReplyKeyboardMarkup(
+        keyboard=[
+            [
+                types.KeyboardButton(text="Пицца 30 см"), 
+                types.KeyboardButton(text="Пицца 40 см")
+            ],
+            [
+                types.KeyboardButton(text="Закуски"),
+            ],
+            [
+                types.KeyboardButton(text="Салаты")
+            ],
+            [
+                types.KeyboardButton(text="Супы"),
+            ]
+        ]
+    )
+    await callback.message.answer("Что пожелаете?", reply_markup=kb)
+
+
+# lambda m: m.text.lower() == "пицца"
+@menu_router.message(F.text.lower() == "пицца 30 см")
+async def show_pizzas(message: types.Message):
+    kb = types.ReplyKeyboardRemove()
+    await message.answer("Вот пиццы, которые мы предлагаем", reply_markup=kb)
+
+
+@menu_router.message(F.text.lower() == "закуски")
+async def show_snacks(message: types.Message):
+    kb = types.ReplyKeyboardRemove()
+    await message.answer("Вот закуски, которые мы предлагаем", reply_markup=kb)
--- main.py
+++ main.py
@@ -5,12 +5,16 @@ from bot import bot, dp
 from handlers.start import start_router
 from handlers.picture import picture_router
 from handlers.echo import echo_router
+from handlers.our_menu import menu_router
+from handlers.survey import survey_router
 
 
 async def main():
     # регистрация роутеров
     dp.include_router(start_router)
     dp.include_router(picture_router)
+    dp.include_router(menu_router)
+    dp.include_router(survey_router)
 
     # в самом конце
     dp.include_router(echo_router)
